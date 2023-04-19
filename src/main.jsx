c96f2d4edb1a96a3cf2433551b61040904c0c564
botino-k <lassmary@bk.ru>
2023-04-20 02:25:03 +0400

    feat: try to deploy5

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App'
-import './index.css'
 
 import Authentication from "./pages/Authentication/Authentication"
 import Database from "./pages/Database/Database"
